1f982c76cc4f8d96a5d5b7a024fc64d9e827aa3a
johnleencist <john.lee.ncist@gmail.com>
2018-12-06 02:33:08 +0800

    + add story service

---
+++ GameCore/Service/StoryService/StoryCmds.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCore
+{
+    class StoryCmds
+    {
+        #region unit
+        public static void UnitAdd() { }
+
+        public static void UnitRemove() { }
+
+        public static void UnitShow() { }
+
+        public static void UnitHide() { }
+        #endregion
+
+        #region obj
+        public static void ObjAdd() { }
+
+        public static void ObjRemove() { }
+
+        public static void ObjShow() { }
+
+        public static void ObjHide() { }
+        #endregion
+    }
+}
