76555b41dff07c1e1cdf6c63a29092f6ced2bcfd
lihsiaochiang <john_lee_ncist@yahoo.com>
2018-12-06 17:45:44 +0800

    + add unity script

---
+++ ClientU3D/Assets/Game/Script/GameRoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameCore;
+
+public class GameRoot : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject.DontDestroyOnLoad(this.gameObject);
+
+        GameConfig.binPath = Application.streamingAssetsPath;
+        GameLog.SetPrintHandler(UnityEngine.Debug.LogFormat);
+        GameContext.InitGame();
+    }
+
+    void Start()
+    {
+        GameContext.StartGame();
+    }
+
+    void Update()
+    {
+        GameContext.TickGame();
+    }
+
+    void OnDestroy()
+    {
+        GameContext.QuitGame();
+    }
+}
